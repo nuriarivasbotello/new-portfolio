118df7eca22da1f3ec78d8df160fb5224ebf606b
Nuria Rivas <129308132+nuriarivasbotello@users.noreply.github.com>
2024-01-31 18:45:18 +0100

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+// El styles lo importamos aquí, ya se carga después al compilar todo
+
+// import '../scss/styles.scss';
+
+// const button = document.getElementById('button');
+
+// button.addEventListener('click', () => {
+//   console.log('BUTTON CLICKED');
+// });
