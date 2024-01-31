b92ba7e2ea55d09c1207c2050b7a69650dd4949d
nuriarivasbotello <nuriarivasbotello@gmail.com>
2024-01-31 21:01:12 +0100

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,25 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 
-// import '../scss/styles.scss';
+import '../scss/styles.scss';
+const menuElement = document.getElementById('menu');
+const buttonElement = document.getElementById('toggle-menu');
+const menuIcon = document.getElementById('menu-icon');
+const mainElement = document.getElementById('main');
 
+function handleClick() {
+  menuElement.classList.toggle('menu--show');
+  if (menuElement.classList.contains('menu--show')) {
+    //  console.log('MENU ABIERTO');
+    mainElement.classList.add('main--show');
+    menuIcon.src = './assets/images/2.png';
+  } else {
+    // console.log('MENU CERRADO');
+    menuIcon.src = './assets/images/1.png';
+    mainElement.classList.remove('main--show');
+  }
+}
+
+buttonElement.addEventListener('click', handleClick);
 // const button = document.getElementById('button');
 
 // button.addEventListener('click', () => {
