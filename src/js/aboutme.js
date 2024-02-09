a4c22f7c79ad3dbf60f1a63540a17009ef85524d
nuriarivasbotello <nuriarivasbotello@gmail.com>
2024-02-09 17:09:03 +0100

    github

--- src/js/aboutme.js
+++ src/js/aboutme.js
@@ -1,19 +1,21 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 
 import '../scss/styles.scss';
+import hamburguesaOn from '/assets/images/hamburguesaon.png';
+import hamburguesaOff from '/assets/images/hamburguesaoff.png';
 const menuElementAbout = document.getElementById('menu-aboutme');
 const menuIconAbout = document.getElementById('menu-icon-about');
-const aboutmeElement = document.getElementById('main');
+const aboutmeElement = document.getElementById('main-about-me');
 
 function handleClick() {
   menuElement.classList.toggle('menu--show');
   if (menuElementAbout.classList.contains('menu--show')) {
     //  console.log('MENU ABIERTO');
     aboutmeElement.classList.add('about--show');
-    menuIconAbout.src = './assets/images/hamburguesaon.png';
+    menuIconAbout = hamburguesaOn;
   } else {
     // console.log('MENU CERRADO');
-    menuIconAbout.src = './assets/images/hamburguesaoff.png';
+    menuIconAbout.src = hamburguesaOff;
     aboutmeElement.classList.remove('about--show');
   }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -6,19 +6,17 @@ import hamburguesaOff from '/assets/images/hamburguesaoff.png';
 const menuElement = document.getElementById('menu');
 const menuIcon = document.getElementById('menu-icon');
 const mainElement = document.getElementById('main');
+
 function handleClick() {
   menuElement.classList.toggle('menu--show');
   if (menuElement.classList.contains('menu--show')) {
     //  console.log('MENU ABIERTO');
     mainElement.classList.add('main--show');
-
     menuIcon.src = hamburguesaOn;
   } else {
     // console.log('MENU CERRADO');
     menuIcon.src = hamburguesaOff;
-
     mainElement.classList.remove('main--show');
   }
 }
-
 menuIcon.addEventListener('click', handleClick);
